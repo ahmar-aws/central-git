661dd4d990b49efa48a0e88bcc755bddf86351de
ahmar <ahmaralikhan2@gmail.com>
2025-08-30 06:45:46 +0000

    prot altered tp 5000

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def home():
     return "Hello from Python App deployed via AWS CodePipeline + CodeDeploy on EC2!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
 
